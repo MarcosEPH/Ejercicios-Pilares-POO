3cbf1ae25ec96b12c367580d25908514d9c11862
MarcosEPH <marcospeerez791@gmail.com>
2025-08-02 19:38:52 -0600

    Add files via upload

---
+++ Ejercicios - Pilares POO/Clases/Ejercicio1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios___Pilares_POO.Clases
+{
+    class Empleado
+    {
+        private string nombre;
+        private double salario;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    nombre = value;
+                }
+            }
+        }
+
+        public double Salario
+        {
+            get { return salario; }
+            set
+            {
+                if (value >= 0)
+                {
+                    salario = value;
+                }
+            }
+        }
+    }
+
+    class CuentaBancaria
+    {
+        private double saldo;
+        private double limiteDiario;
+
+        public CuentaBancaria(double saldoInicial, double limite)
+        {
+            saldo = saldoInicial;
+            limiteDiario = limite;
+        }
+
+        public void Retirar(double cantidad)
+        {
+            if (cantidad <= saldo && cantidad <= limiteDiario)
+            {
+                saldo -= cantidad;
+                System.Console.WriteLine("Retiro exitoso");
+            }
+            else
+            {
+                System.Console.WriteLine("No se puede retirar esa cantidad");
+            }
+        }
+
+        public void MostrarSaldo()
+        {
+            System.Console.WriteLine("Saldo disponible: Q" + saldo);
+        }
+    }
+
+    class Vehiculo
+    {
+        private bool encendido;
+
+        public bool Encendido
+        {
+            get { return encendido; }
+        }
+
+        public void Encender()
+        {
+            encendido = true;
+            System.Console.WriteLine("Vehículo encendido");
+        }
+
+        public void Apagar()
+        {
+            encendido = false;
+            System.Console.WriteLine("Vehículo apagado");
+        }
+    }
+}
---
+++ Ejercicios - Pilares POO/Program.cs
@@ -0,0 +1,41 @@
+﻿using Ejercicios___Pilares_POO.Clases;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(" EMPLEADO ");
+        Empleado emp = new Empleado();
+        Console.Write("Ingrese nombre: ");
+        emp.Nombre = Console.ReadLine();
+        Console.Write("Ingrese salario: ");
+        emp.Salario = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Nombre: " + emp.Nombre);
+        Console.WriteLine("Salario: Q" + emp.Salario);
+
+        Console.WriteLine("\n CUENTA BANCARIA ");
+        Console.Write("Saldo inicial: ");
+        double saldo = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Límite de retiro diario: ");
+        double limite = Convert.ToDouble(Console.ReadLine());
+        CuentaBancaria cuenta = new CuentaBancaria(saldo, limite);
+        Console.Write("Cantidad a retirar: ");
+        double retiro = Convert.ToDouble(Console.ReadLine());
+        cuenta.Retirar(retiro);
+        cuenta.MostrarSaldo();
+
+        Console.WriteLine("\n VEHÍCULO ");
+        Vehiculo carro = new Vehiculo();
+        Console.Write("¿Encender vehículo? (s/n): ");
+        string opcion = Console.ReadLine();
+        if (opcion == "s")
+        {
+            carro.Encender();
+        }
+        else
+        {
+            carro.Apagar();
+        }
+        Console.WriteLine("¿Está encendido? " + carro.Encendido);
+    }
+}
